b2aeea5cf80ce7057fd6419efdd1ec6da99af458
StephanieEarl <77116494+StephanieEarl@users.noreply.github.com>
2024-10-24 11:24:21 +0100

    Further testing on branch

--- api_endpoints_test.py
+++ api_endpoints_test.py
@@ -13,6 +13,19 @@ class TestApiEndpoints(unittest.TestCase):
         # Check the response status code is 200
         self.assertEqual(response.status_code, 200)
 
+        # Test if the response is in JSON format
+        self.assertTrue(response.is_json)
+
+        # Test if the response contains the expected keys
+        self.assertTrue("dates" in response.json)
+        self.assertTrue("attended" in response.json)
+
+        # Test if the dates response is in the list
+        self.assertIsInstance(response.json["dates"], list)
+
+        # Test to see if attended response is an integer
+        self.assertTrue(all(isinstance(x, int) for x in response.json["attended"]))
+
 
 if __name__ == "__main__":
     unittest.main()
